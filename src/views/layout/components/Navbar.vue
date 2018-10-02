5cd2df7946ec215d7f292925503def8295b6d891
manazpz <1348319927@qq.com>
2018-10-02 19:49:37 +0800

    v20181002

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -19,7 +19,7 @@
 
       <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
-          <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+          <img class="user-avatar" :src="avatar">
           <i class="el-icon-caret-bottom"></i>
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -105,7 +105,7 @@
       cropSuccess(resData) {
         this.imagecropperShow = false
         this.imagecropperKey = this.imagecropperKey + 1
-        this.$store.dispatch('setAvatar', resData.headImg)
+        this.$store.dispatch('setAvatar', resData.headPortrait)
       },
       close() {
         this.imagecropperShow = false
