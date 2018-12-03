29772e37b9860986469c89246d6cefcaa97f59b2
manayxw <215828718@qq.com>
2018-12-03 17:49:06 +0800

    20181203

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"http://47.106.177.213:8083/aq"',
   RESOURCE_URL: '"http://47.106.177.213"',
+  // BASE_API: '"http://xa.huanyibu.com/aq"',
+  // RESOURCE_URL: '"http://wx.huanyibu.com"',
 }
 
